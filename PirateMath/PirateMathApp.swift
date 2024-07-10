54d96f900f58eb927627a36d9000882752269fb3
Ievgen Kuznietsov <ievgen.contact@gmail.com>
2024-07-10 13:05:33 +0100

    Initial Commit

---
+++ PirateMath/PirateMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  PirateMathApp.swift
+//  PirateMath
+//
+//  Created by Mac on 10/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct PirateMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
